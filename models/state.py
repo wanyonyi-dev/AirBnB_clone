6db0efcf6fe4cbe27061a44028047d628f155134
wanyonyi-dev <wanyonyip148@gmail.com>
2024-03-23 04:45:30 -0700

    task 1

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create a Module for State class."""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class representing a State."""
+    name = ""
